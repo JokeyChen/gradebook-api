b43914d05b5895205b0c6a3eda2f8d1115a9e465
JokeyChen <jokey.chen@gmail.com>
2017-03-26 16:15:52 -0400

    Support default weight

--- database/grade.js
+++ database/grade.js
@@ -12,6 +12,12 @@ var defaultScale = [
   ['F', 0]
 ];
 
+var defaultWeight = {
+  homeworks: 0.3,
+  quizzes: 0.2,
+  exams: 0.5
+};
+
 function listenForGradeChange() {
 
   // Update courses totals when the value of homeworks, quizzes, and exams changed
@@ -65,40 +71,60 @@ function listenForGradeChange() {
 
   // HELPER METHODS
   function calculateGrade(courseId) {
-    var totalEarnedScore = 0;
-    var totalMaxScore = 0;
+    var numeric = 0;
     // homeworks
     db.homeworksRef.orderByChild('course').equalTo(courseId).once('value', function (snapshot) {
+      var totalEarnedScore = 0;
+      var totalMaxScore = 0;
       snapshot.forEach(function (homework) {
         totalEarnedScore += homework.val().earnedScore;
         totalMaxScore += homework.val().maxScore;
       });
+      var percentage = totalEarnedScore / totalMaxScore;
+      if (!isNaN(percentage)) {
+        numeric += percentage * defaultWeight.homeworks * 100;
+      } else {
+        numeric += defaultWeight.homeworks * 100;
+      }
     });
     // quizzes
     db.quizzesRef.orderByChild('course').equalTo(courseId).once('value', function (snapshot) {
+      var totalEarnedScore = 0;
+      var totalMaxScore = 0;
       snapshot.forEach(function (quiz) {
         totalEarnedScore += quiz.val().earnedScore;
         totalMaxScore += quiz.val().maxScore;
       });
+      var percentage = totalEarnedScore / totalMaxScore;
+      if (!isNaN(percentage)) {
+        numeric += percentage * defaultWeight.quizzes * 100;
+      } else {
+        numeric += defaultWeight.quizzes * 100;
+      }
     });
     // exams
     db.examsRef.orderByChild('course').equalTo(courseId).once('value', function (snapshot) {
+      var totalEarnedScore = 0;
+      var totalMaxScore = 0;
       snapshot.forEach(function (exam) {
         totalEarnedScore += exam.val().earnedScore;
         totalMaxScore += exam.val().maxScore;
       });
+      var percentage = totalEarnedScore / totalMaxScore;
+      if (!isNaN(percentage)) {
+        numeric += percentage * defaultWeight.exams * 100;
+      } else {
+        numeric += defaultWeight.exams * 100;
+      }
     });
     var updates = {};
-    if (totalMaxScore == 0) {
-      // write numeric grade
-      updates['/courses/' + courseId + '/numericGrade'] = 100;
+    if (numeric == 0) {
       // write letter grade
       updates['/courses/' + courseId + '/letterGrade'] = 'A';
+      // write numeric grade
+      updates['/courses/' + courseId + '/numericGrade'] = 100;
       db.ref.update(updates);
     } else {
-      var percentage = (totalEarnedScore / totalMaxScore).toPrecision(2) * 100;
-      // write numeric grade
-      updates['/courses/' + courseId + '/numericGrade'] = percentage;
       var courseScale = defaultScale;
       // try to retrieve course specific scale
       db.coursesRef.child(courseId + '/scale').once('value', function (snapshot) {
@@ -114,16 +140,18 @@ function listenForGradeChange() {
             });
           });
         }
-        // write letter grade
         var letter = '';
         for (var i = 0; i < courseScale.length; i++) {
           var scale = courseScale[i];
-          if (percentage >= scale[1]) {
+          if (numeric >= scale[1]) {
             letter = scale[0];
             break;
           }
         }
+        // write letter grade
         updates['/courses/' + courseId + '/letterGrade'] = letter;
+        // write numeric grade
+        updates['/courses/' + courseId + '/numericGrade'] = numeric.toFixed(2);
         db.ref.update(updates);
       });
     }
